6ec3a3876378753de5ee585a2322b509571807ed
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2024-03-11 20:17:55 +0100

    refactor: clean code

--- task_1.py
+++ task_1.py
@@ -1,60 +1,49 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
 
+# List for dictionary keys, there is no need to put weekend days
+WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
-# Function for checking if day is on the weekend
+# Offset for calculating start of the next weekd
+WEEK_MAX_OFFSET = 7
+
+
 def is_day_weekend(date):
     return True if date.weekday() == 5 or date.weekday() == 6 else False
 
 
-# Function for calculating birthday celebrations on next weekend
 def get_birthday_per_week(users):
-    # List for dictionary keys, there is no need to put weekend days
-    WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-
-    # Offset for calculating start of the next weekd
-    WEEK_MAX_OFFSET = 7
-
-    # Dictionary with users
     users_dict = defaultdict(list)
-
-    # Current date
     current_date = datetime.today().date()
-    # current_date = datetime(2024, 12, 30).date()    # test for checking a date at the end of a year
 
     for user in users:
         name = user["name"]
-
-        # Removing the time part
         celebration_day = user["birthday"].date()
 
-        # Change celebbration date to actual year
+        # Change celebration date to actual year
         celebration_day = celebration_day.replace(year=current_date.year)
 
-        # Calculate start of next week
         celebration_week_start_date = current_date + timedelta(days=WEEK_MAX_OFFSET - current_date.weekday())
         celebration_week_day_offset = (celebration_week_start_date - celebration_day).days
 
-        # If celebration date is older than current, assign celebration for next year
-        # Date year on weekends just before celebration date (in same year) cannot be incremented, because they are assigned to the next week
+        # If celebration date is older than current, assign it to next year
+        # Date year on weekend just before celebration date (in same year) cannot be incremented, because theese dates are assigned to the next week
         if celebration_week_day_offset > 2:    
             celebration_day = celebration_day.replace(year=celebration_day.year + 1)
         
         celebration_day_offset = (celebration_day - celebration_week_start_date).days
 
-        # If the day difference between celebration date and start of next week is more than 4 (4 == Friday), there is no point for further calculations
+        # If the difference is more than 4 (4 == Friday), there is no point for further calculations
         if celebration_day_offset > 4:
             continue
         
-        # If the day is on weekend, add proper offset to set it to next Monday
+        # If the day is on weekend, set it to next Monday
         if(is_day_weekend(celebration_day)):
             celebration_weekend_offset = WEEK_MAX_OFFSET - celebration_day.weekday()
             celebration_day = celebration_day + timedelta(days=celebration_weekend_offset)
 
-        # At this point date should be within the next week, so there is no need for further conditions
         users_dict[WEEK_DAYS[celebration_day.weekday()]].append(name)
     
-    # Print data
     if not users_dict:
         print('No celebration next week :(')
     else:
@@ -64,17 +53,14 @@ def get_birthday_per_week(users):
             if users_dict[day]:
                 print(f'{day:<10}: {', '.join(users_dict[day])}')
 
-    
-############ TEST CALL ############
-'''
+
 users = [{"name": "Bill Gates", "birthday": datetime(1955, 3, 2),},
          {"name": "Bill a", "birthday": datetime(1955, 3, 3)},
          {"name": "Bill b", "birthday": datetime(1955, 3, 15)},
          {"name": "Bill c", "birthday": datetime(1955, 3, 16)},
-         {"name": "Bill d", "birthday": datetime(1955, 3, 9)},
+         {"name": "Bill d", "birthday": datetime(1955, 3, 20)},
          {"name": "Bill e", "birthday": datetime(1955, 3, 10)},
          {"name": "Bill f", "birthday": datetime(1955, 3, 13)},
          {"name": "Bill f", "birthday": datetime(1955, 12, 30)}]
 
 get_birthday_per_week(users)
-'''
--- task_2.py
+++ task_2.py
@@ -4,88 +4,74 @@ def parse_input(user_input):
     return cmd, *args
 
 def add_contact(args, contacts):
-    # Check number of arguments
     if len(args) != 2:
         return 'Wrong number of arguments. Try again!'
-    else:
-        name, phone = args
 
-        # Check if key exists
-        if contacts.get(name, False):
-            return 'Contact already exists! If you want to change contact, use "change" command.'
-        else:
-            contacts[name] = phone
-            return 'Contact added.'
+    name, phone = args
+
+    if contacts.get(name, False):
+        return 'Contact already exists! If you want to change contact, use "change" command.'
+ 
+    contacts[name] = phone
+    return 'Contact added.'
         
 def change_contact(args, contacts):
-    # Check number of arguments
     if len(args) != 2:
         return 'Wrong number of arguments. Try again!'
-    else:
-        name, phone = args
 
-        # Check if key exists
-        if contacts.get(name, False):
-            contacts[name] = phone
-            return 'Contact changed.'
-        else:
-            return 'Contact does not exists! If you want to add contact, use "add" command.'
+    name, phone = args
+
+    if contacts.get(name, False):
+        contacts[name] = phone
+        return 'Contact changed.'
+        
+    return 'Contact does not exists! If you want to add contact, use "add" command.'
         
 def get_phone(args, contacts):
-    # Check number of arguments
     if len(args) != 1:
         return 'Wrong number of arguments. Try again!'
-    else:
-        (name, ) = args
 
-        # Check if key exists
-        if contacts.get(name, False):
-            return f'Phone number for {name}: {contacts[name]}'
-        else:
-            return 'Contact does not exists! If you want to add contact, use "add" command.'
+    (name, ) = args
+
+    if contacts.get(name, False):
+        return f'Phone number for {name}: {contacts[name]}'
+        
+    return 'Contact does not exists! If you want to add contact, use "add" command.'
         
 def get_all(args, contacts):
-    # Check number of arguments
     if len(args) != 0:
         return 'You should not put any arguments in "all" command!'
+    
     if not contacts:
         return 'No contacts in phonebook!!!'
-    else:
-        contact_name = 'Contact'
 
-        # Calculate max width of contact column
-        max_key_len = max(len(key) for key in contacts)
-        max_key_len = len(contact_name) if len(contact_name) > max_key_len else max_key_len
-        max_key_len = max_key_len + 1 if not max_key_len % 2 else max_key_len
+    contact_name = 'Contact'
+    max_key_len = max(len(key) for key in contacts)
+    max_key_len = len(contact_name) if len(contact_name) > max_key_len else max_key_len
+    max_key_len = max_key_len + 1 if not max_key_len % 2 else max_key_len
 
-        # Calculate max width of phone number column
-        phone_name = 'Phone'
-        max_contact_len = max(len(contacts[key]) for key in contacts)
-        max_contact_len = len(phone_name) if len(phone_name) > max_contact_len else max_contact_len
-        max_contact_len = max_contact_len + 1 if not max_contact_len % 2 else max_contact_len
+    phone_name = 'Phone'
+    max_contact_len = max(len(contacts[key]) for key in contacts)
+    max_contact_len = len(phone_name) if len(phone_name) > max_contact_len else max_contact_len
+    max_contact_len = max_contact_len + 1 if not max_contact_len % 2 else max_contact_len
 
-        # Prepare separator for rows
-        space_width = 4
-        separator = f'|{'-' * ((max_key_len + max_contact_len) + 2 * space_width + 1)}|\n'
+    space_width = 4
+    separator = f'|{'-' * ((max_key_len + max_contact_len) + 2 * space_width + 1)}|\n'
 
-        # Print phonebook
-        # Print title
-        contacts_string = f'\n{separator}'
-        title_name = 'PHONEBOOK'
-        pound_sign_string_len = int((len(separator) - len(title_name)) / 2)
-        contacts_string += f'| {'#' * (pound_sign_string_len - 3)} {title_name} {'#' * (pound_sign_string_len - 3)} |\n'
-        contacts_string += separator
+    contacts_string = f'\n{separator}'
+    title_name = 'PHONEBOOK'
+    pound_sign_string_len = int((len(separator) - len(title_name)) / 2)
+    contacts_string += f'| {'#' * (pound_sign_string_len - 3)} {title_name} {'#' * (pound_sign_string_len - 3)} |\n'
+    contacts_string += separator
         
-        # Print column names
-        contacts_string += '|{name:^{name_width}}|{phone:^{phone_width}}|\n'.format(name=contact_name, name_width=max_key_len + space_width, phone=phone_name, phone_width=max_contact_len + space_width)
-        contacts_string += separator * 2
+    contacts_string += '|{name:^{name_width}}|{phone:^{phone_width}}|\n'.format(name=contact_name, name_width=max_key_len + space_width, phone=phone_name, phone_width=max_contact_len + space_width)
+    contacts_string += separator * 2
 
-        # Print contacts
-        for key in contacts:
-            contacts_string += '|{name:^{name_width}}|{phone:^{phone_width}}|\n'.format(name=key, name_width=max_key_len + 4, phone=contacts[key], phone_width=max_contact_len + 4)
-            contacts_string += separator
+    for key in contacts:
+        contacts_string += '|{name:^{name_width}}|{phone:^{phone_width}}|\n'.format(name=key, name_width=max_key_len + 4, phone=contacts[key], phone_width=max_contact_len + 4)
+        contacts_string += separator
 
-        return contacts_string
+    return contacts_string
 
 def main():
     contacts = {}
